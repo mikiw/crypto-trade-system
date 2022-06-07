ea215cf6e636f924cdf85273e0f14ab88aff8d1c
mikiw <mikiw.pp@gmail.com>
2022-06-07 18:54:03 +0200

    Print values.

--- eth_investing_calculator/src/main.rs
+++ eth_investing_calculator/src/main.rs
@@ -6,7 +6,7 @@ type Error = Box<dyn std::error::Error>;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
 #[derive(Deserialize, Debug)]
-struct Response {
+struct Transactions {
     status: String,
     message: String,
     result: Vec<Transaction>,
@@ -34,29 +34,43 @@ struct Transaction {
     confirmations: String,
 }
 
-async fn get_data() -> Result<()> {
+async fn get_data(address: String, api_key: String) -> Result<(Transactions)> {
     let request_url = format!("http://api.etherscan.io/api?module=account&action=txlist&sort=desc&address={address}&apikey={api_key}",
-                            address = "",
-                            api_key = "");
+                            address = address,
+                            api_key = api_key);
 
     let http_response = reqwest::get(request_url).await?;
-    let response = http_response.json::<Response>().await?;
-    println!("{:#?}", response);
+    let response = http_response.json::<Transactions>().await?;
 
-    Ok(())
+    Ok(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Input your ETH address!");
-    let mut address = String::new();
+    // TODO: Convert eth value as decimal value.
+    // TODO: Calculate inputs and outputs.
+    // TODO: Get eth value as dollar in from timeStamp like 1640947396 UnixToDate.
+
+    let mut api_key = String::from("");
+    let mut address = String::from("0xc55dbe3cd4afa41e8c24283c5be8d2481e2b79c1");
+
+    println!("Input your ETH address or press enter to get transactions from {} address.", address);
     io::stdin()
         .read_line(&mut address)
         .expect("Failed to read line");
 
     println!("Your address: {}", address);
 
-    get_data().await?;
+    let data = get_data(address, api_key).await?;
+
+    for i in data.result {
+        println!("Transaction: ");
+        println!("{:#?}", i.timeStamp);
+        println!("{:#?}", i.value);
+        println!("{:#?}", i.from);
+        println!("{:#?}", i.to);
+        println!("");
+    }
 
     Ok(())
 }
