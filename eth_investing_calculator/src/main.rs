e60030dcd22f5fa153e2bf73f5a0a8daffee0a23
mikiw <mikiw.pp@gmail.com>
2022-06-10 11:47:21 +0200

    Add buy and sell functions.

--- eth_investing_calculator/src/main.rs
+++ eth_investing_calculator/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use serde::Deserialize;
 use chrono::prelude::*;
+use std::{thread, time::Duration};
 
 extern crate ether_converter;
 
@@ -62,7 +63,56 @@ fn timestamp_to_date(unix_timestamp: String) -> DateTime<Utc> {
     datetime
 }
 
-async fn get_transactions(address: String, api_key: String) -> Result<Transactions> {
+async fn buy(transactions: Transactions, address: &str) -> Result<(f32, f32)> {
+
+	let mut sum_buy_value = 0.0;
+	let mut sum_buy_usd = 0.0;
+
+    for t in transactions.result {
+        if t.to == address {
+            let price = get_price(t.timeStamp).await?;
+            let map = ether_converter::convert(&t.value, "wei");
+            let value: f32 = map.get("ether").unwrap().parse().unwrap(); // TODO: change to match to avoid panic
+
+            sum_buy_value += value;
+            sum_buy_usd += price.market_data.current_price.usd * value;
+
+            println!("buy value in usd: {:#?}", price.market_data.current_price.usd * value);
+        }
+    }
+
+    println!("sum_buy_value: {:#?}", sum_buy_value);
+    println!("sum_buy_usd: {}", sum_buy_usd);
+
+	Ok((sum_buy_value, sum_buy_usd))
+}
+
+async fn sell(transactions: Transactions, address: &str) -> Result<(f32, f32)> {
+
+	let mut sum_sell_value = 0.0;
+	let mut sum_sell_usd = 0.0;
+
+    for t in transactions.result {
+        if t.from == address {
+            let price = get_price(t.timeStamp).await?;
+            let map = ether_converter::convert(&t.value, "wei");
+            let value: f32 = map.get("ether").unwrap().parse().unwrap(); // TODO: change to match to avoid panic
+
+            sum_sell_value += value;
+            sum_sell_usd += price.market_data.current_price.usd * value;
+
+            println!("sell value in usd: {:#?}", price.market_data.current_price.usd * value);
+        }
+    }
+
+    println!("sum_sell_value: {:#?}", sum_sell_value);
+    println!("sum_sell_usd: {}", sum_sell_usd);
+
+	Ok((sum_sell_value, sum_sell_usd))
+}
+
+async fn get_transactions(address: &str, api_key: &str) -> Result<Transactions> {
+
     let request_url = format!("http://api.etherscan.io/api?module=account&action=txlist&sort=desc&address={address}&apikey={api_key}",
                             address = address,
                             api_key = api_key);
@@ -74,43 +124,32 @@ async fn get_transactions(address: String, api_key: String) -> Result<Transactio
 }
 
 async fn get_price(unix_timestamp: String) -> Result<EthPrice> {
+
     let request_url = format!("https://api.coingecko.com/api/v3/coins/ethereum/history?date={date}",
                             date = timestamp_to_date(unix_timestamp).format("%d-%m-%Y"));
-
     let http_response = reqwest::get(request_url).await?;
     let response = http_response.json::<EthPrice>().await?;
+    
+    thread::sleep(Duration::from_millis(1000)); // to avoid HTTP 429 [Too Many Requests]
 
     Ok(response)
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // TODO: Calculate inputs (buy) and outputs (sell).
 
-    let mut api_key = String::from("");
-    let mut address = String::from("0xc55dbe3cd4afa41e8c24283c5be8d2481e2b79c1");
-
-    println!("Input your ETH address or press enter to get transactions from {} address.", address);
-    io::stdin()
-        .read_line(&mut address)
-        .expect("Failed to read line");
+    let mut api_key = "";
+    let mut address = "0xc55dbe3cd4afa41e8c24283c5be8d2481e2b79c1";
 
     println!("Your address: {}", address);
 
     let transactions = get_transactions(address, api_key).await?;
-    
-    for i in transactions.result {
-        println!("Transaction: ");
-        println!("{:#?}", i.from);
-        println!("{:#?}", i.to);
-        let price = get_price(i.timeStamp).await?;
-        println!("{:#?}", price);
+    let buy = buy(transactions, address).await?;
 
-        let map = ether_converter::convert(&i.value, "wei");
-        let val = map.get("ether").unwrap();
-    
-        println!("{}", val);
-    }
+    let transactions2 = get_transactions(address, api_key).await?; // TODO: remove this ownership problem
+    let sell = sell(transactions2, address).await?;
+
+    println!("Income in USD: {}", sell.1 - buy.1);
 
     Ok(())
 }
